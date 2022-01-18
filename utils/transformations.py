6c38d61d51c7637a936f1e763ac64ac70ac59908
youssef <youssefallouah@gmail.com>
2022-01-18 16:36:41 +0100

    add new transformations types

--- utils/transformations.py
+++ utils/transformations.py
@@ -7,11 +7,17 @@ class Transform:
 
 
 class AffineTransform(Transform):
-    NAME2TUPLE = {"identity": (lambda x: 1, lambda x: 0),
-                  "standardization": (lambda x: 1/np.std(x), lambda x: -np.mean(x)/np.std(x)),
-                  "min-max": (lambda x: 1/(max(x)-min(x)), lambda x: -min(x)/(max(x)-min(x))),
-                  "mean-max": (lambda x: 1/(max(x)-min(x)), lambda x: -np.mean(x)/(max(x)-min(x)))
-                  }
+    NAME2TUPLE = {
+        "identity": (lambda x: 1, lambda x: 0),
+        "standardization": (lambda x: 1 / np.std(x), lambda x: -np.mean(x) / np.std(x)),
+        "min-max": (lambda x: 1 / (max(x) - min(x)), lambda x: -min(x) / (max(x) - min(x))),
+        "mean-max": (lambda x: 1 / (max(x) - min(x)), lambda x: -np.mean(x) / (max(x) - min(x))),
+        "median-quartile": (lambda x: 1 / (np.quantile(x, .75) - np.quantile(x, .25)),
+                            lambda x: - np.median(x) / (np.quantile(x, .75) - np.quantile(x, .25))),
+        "l1-norm": (lambda x: 1 / norm(x, p=1), lambda x: 0),
+        "l2-norm": (lambda x: 1 / norm(x, p=2), lambda x: 0),
+        "l2-proj": (lambda x: 1 / norm(x, p=2), lambda x: -np.mean(x) / norm(x, p=2))
+    }
 
     def __init__(self, name="identity"):
         self.slope, self.offset = AffineTransform.NAME2TUPLE[name]
@@ -25,3 +31,8 @@ class AffineTransform(Transform):
         offset = self.offset([x for i, x in enumerate(param) if mask[i] != 0])
         out = slope * param + offset
         return out
+
+
+def norm(x, p=2):
+    out = sum(np.abs(x) ** p) ** (1/p)
+    return out
