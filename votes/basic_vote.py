dffcc0b14e5f313f9ba4de376b15e4df28d51655
youssef <youssefallouah@gmail.com>
2022-01-24 21:21:48 +0100

    fix qr_median argument (voting_resilience) passing

--- votes/basic_vote.py
+++ votes/basic_vote.py
@@ -6,9 +6,10 @@ from votes.vote import Vote
 from utils.transformations import AffineTransform
 from utils.optimizers import dichotomy
 
+
 class BasicVote(Vote):
 
-    def __init__(self, ratings, mask, voting_rights, voting_resilience=1,  
+    def __init__(self, ratings, mask, voting_rights, voting_resilience=1,
                  transformation_name='standardization'):
         super().__init__(ratings, mask, voting_rights)
         self.voting_resilience = voting_resilience  # W in the paper
@@ -20,7 +21,7 @@ class BasicVote(Vote):
         delta = 1
 
         bnds = (min(0, min(scores)), max(0, max(scores)))
-        out2 = dichotomy(weights, scores, delta=delta, voting_resilience=self.voting_resilience, bnds=bnds)
+        out2 = dichotomy(weights, scores, delta=delta, voting_resilience=voting_resilience, bnds=bnds)
 
         return out2
 
@@ -34,7 +35,8 @@ class BasicVote(Vote):
 
         for alternative in range(self.n_alternatives):
             scores = np.array(
-                [x for voter, x in enumerate(self.ratings[:, alternative]) if self.mask[voter][alternative] != 0]).reshape(-1, 1)
+                [x for voter, x in enumerate(self.ratings[:, alternative]) if
+                 self.mask[voter][alternative] != 0]).reshape(-1, 1)
             weights = np.array(
                 [x for voter, x in enumerate(self.voting_rights) if self.mask[voter][alternative] != 0]).reshape(-1, 1)
             out[alternative] = self.qr_median(scores, weights)
