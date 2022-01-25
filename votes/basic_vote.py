987ebeef3ccb58cbbd32fb8bb7fe938e7e80810f
youssef <youssefallouah@gmail.com>
2022-01-25 09:46:26 +0100

    change qr_median logic (use Dichotomy class)

--- votes/basic_vote.py
+++ votes/basic_vote.py
@@ -1,28 +1,40 @@
 """ Implementation of BasicVote """
 
 import numpy as np
+from numpy.distutils.system_info import x11_info
 
 from votes.vote import Vote
 from utils.transformations import AffineTransform
-from utils.optimizers import dichotomy
+from utils.optimizers import ShelfOptimizer, Dichotomy, derivate
+
 
 class BasicVote(Vote):
+    NAME2OPT = {
+        "shelf": ShelfOptimizer,
+        "dichotomy": Dichotomy
+    }
 
-    def __init__(self, ratings, mask, voting_rights, voting_resilience=1,  
+    def __init__(self, ratings, mask, voting_rights, voting_resilience=1,
                  transformation_name='standardization'):
         super().__init__(ratings, mask, voting_rights)
         self.voting_resilience = voting_resilience  # W in the paper
         self.transformation = AffineTransform(name=transformation_name)
 
-    def qr_median(self, scores, weights, voting_resilience=None):
+    def qr_median(self, scores, weights, voting_resilience=None, opt_name="dichotomy"):
         if voting_resilience is None:
             voting_resilience = self.voting_resilience
-        delta = 1
 
-        bnds = (min(0, min(scores)), max(0, max(scores)))
-        out2 = dichotomy(weights, scores, delta=delta, voting_resilience=self.voting_resilience, bnds=bnds)
+        delta = 1.
+        bounds = ((min(0, min(scores)), max(0, max(scores))),)
+        optimizer = BasicVote.NAME2OPT[opt_name](tolerance=1e-9, max_iter=100)
+        function = lambda x: 0.5 * voting_resilience * x ** 2 + (weights.T @ np.abs(x - scores)).sum()
+        derivative = None
+        if opt_name == "dichotomy":
+            derivative = lambda x: derivate(x, weights, scores, delta, voting_resilience)
+        # if opt_name == "dichotomy", the argument 'function' passed to minimize is useless
+        out = optimizer.minimize(function=function, derivative=derivative, bounds=bounds)
 
-        return out2
+        return out
 
     def run(self):
         """ run voting algorithm """
@@ -34,7 +46,8 @@ class BasicVote(Vote):
 
         for alternative in range(self.n_alternatives):
             scores = np.array(
-                [x for voter, x in enumerate(self.ratings[:, alternative]) if self.mask[voter][alternative] != 0]).reshape(-1, 1)
+                [x for voter, x in enumerate(self.ratings[:, alternative]) if
+                 self.mask[voter][alternative] != 0]).reshape(-1, 1)
             weights = np.array(
                 [x for voter, x in enumerate(self.voting_rights) if self.mask[voter][alternative] != 0]).reshape(-1, 1)
             out[alternative] = self.qr_median(scores, weights)
