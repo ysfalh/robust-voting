a8919c3c7dc96db9d67e2f601891d59b2c288a4b
youssef <youssefallouah@gmail.com>
2022-01-28 12:09:48 +0100

    fix reproducibility across parameter values in auto_run

--- tests/compare.py
+++ tests/compare.py
@@ -4,35 +4,36 @@ from data_generation.voting_rights import generate_voting_rights, regularize_vot
 from votes.mehestan import Mehestan
 from votes.basic_vote import BasicVote
 from plots.boxplot import draw_curves, range_boxplot
+from numpy.random import default_rng
 import json
 
 
 def comparative_runs(
-    n_attempts=1, n_voters=30, n_extreme=0, n_alternatives=200, 
-    density=.01, noise=0, p_byzantine=.45, byz_density=1., voting_resilience=1.,
-     transformation_name="min-max", regularize=True, pair_perc=1., **kwargs
-    ):
-    """ comparing the voting algorithms on generated data """ 
+        n_attempts=1, n_voters=30, n_extreme=0, n_alternatives=200,
+        density=.01, noise=0, p_byzantine=.45, byz_density=1., voting_resilience=1.,
+        transformation_name="min-max", regularize=True, pair_perc=1., rng=None, **kwargs
+):
+    """ comparing the voting algorithms on generated data """
     bv_corr, bv_p, bv_noreg_corr, bv_noreg_p, mh_corr, mh_p = [], [], [], [], [], []
     for i in range(n_attempts):
-        
+
         # data generation
         ratings, original_preferences, mask = generate_data(
             n_voters, n_extreme, n_alternatives, noise=noise,
-            density=density, byz_density=byz_density, pair_perc=pair_perc, **kwargs
+            density=density, byz_density=byz_density, pair_perc=pair_perc, rng=rng, **kwargs
         )
-        voting_rights = generate_voting_rights(n_voters, p_byzantine, **kwargs)
+        voting_rights = generate_voting_rights(n_voters, p_byzantine, rng=rng, **kwargs)
         if regularize:
             voting_rights, mask = regularize_voting_rights(
-                original_preferences, voting_rights, mask, 
-                voting_resilience=voting_resilience, **kwargs
+                original_preferences, voting_rights, mask,
+                voting_resilience=voting_resilience, rng=rng, **kwargs
             )
             d = mask.sum() / (n_voters * n_alternatives)
             # print("Density after regularization: {}".format(d))
 
         # voting with Basic Vote
         bv = BasicVote(
-            ratings, mask, voting_rights, 
+            ratings, mask, voting_rights,
             voting_resilience, transformation_name=transformation_name
         )
         out, out_noreg = bv.run()
@@ -46,7 +47,7 @@ def comparative_runs(
         bv_noreg_p.append(pval)
 
         # voting with Mehestan
-        mh = Mehestan(ratings, mask, voting_rights, voting_resilience)
+        mh = Mehestan(ratings, mask, voting_rights, voting_resilience, transformation_name=transformation_name)
         out = mh.run()
         corr, pval = pearsonr(out, original_preferences)
         mh_corr.append(corr)
@@ -55,14 +56,15 @@ def comparative_runs(
     return bv_corr, bv_p, bv_noreg_corr, bv_noreg_p, mh_corr, mh_p
 
 
-def auto_run(dic={}, rng=None, **kwargs):
+def auto_run(dic={}, seed=1, **kwargs):
     """ multiple runs of both algorithms with 1 parameter changing """
     l_bv_corr, l_bv_p, l_bv_noreg_corr, l_bv_noreg_p, l_mh_corr, l_mh_p = [], [], [], [], [], []
     for name, values in kwargs.items():  # only 1 iteration
         for param in values:
             print(name, ':', param)
             dic.pop(name, None)  # remove parameter default value
-            bv_corr, bv_p, bv_noreg_corr, bv_noreg_p, mh_corr, mh_p = comparative_runs(**{name:param}, **dic, rng=rng)
+            rng = default_rng(seed)
+            bv_corr, bv_p, bv_noreg_corr, bv_noreg_p, mh_corr, mh_p = comparative_runs(**{name: param}, **dic, rng=rng)
             l_bv_corr.append(bv_corr)
             l_bv_p.append(bv_p)
             l_bv_noreg_corr.append(bv_noreg_corr)
@@ -77,11 +79,13 @@ def write_params(params, path='params.json'):
         json.dump(params, f)
 
 
-def run_plot(dic={}, rng=None, **kwargs):
+def run_plot(dic={}, seed=1, **kwargs):
     write_params(dic, path='results/params.json')
     for name, values in kwargs.items():  # only 1 iteration
-        l_bv_corr, l_bv_p, l_bv_noreg_corr, _l_bv_noreg_p, l_mh_corr, l_mh_p = auto_run(rng=rng, dic=dic, **kwargs)
-        draw_curves(l_bv_corr, l_bv_noreg_corr, l_mh_corr, values, labels=('BasicVote+QrMed', 'BasicVote', 'Mehestan'), x_name=name)
+        l_bv_corr, l_bv_p, l_bv_noreg_corr, _l_bv_noreg_p, l_mh_corr, l_mh_p = auto_run(seed=seed, dic=dic, **kwargs)
+        draw_curves(l_bv_corr, l_bv_noreg_corr, l_mh_corr, values, labels=('BasicVote+QrMed', 'BasicVote', 'Mehestan'),
+                    x_name=name)
         range_boxplot(l_bv_corr, values, title='BasicVote+QrMed', x_name=name)
         range_boxplot(l_bv_noreg_corr, values, title='BasicVote', x_name=name)
         range_boxplot(l_mh_corr, values, title='Mehestan', x_name=name)
+    print("++DONE++")
