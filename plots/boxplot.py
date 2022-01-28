e5cb95e2cd61f81cfec3055122885a294aeed607
youssef <youssefallouah@gmail.com>
2022-01-28 11:19:42 +0100

    change algos' names

--- plots/boxplot.py
+++ plots/boxplot.py
@@ -2,11 +2,12 @@ import matplotlib.pyplot as plt
 import numpy as np
 import math
 
+
 def disp_boxplot(bv_corr, bv_p, mh_corr, mh_p, labels=None, whis=None):
     """ display boxplot of correlations and p_values """
     figure, axis = plt.subplots(1, 2)
-    # axis[0].set_ylim([0, 1])
-    # axis[1].set_ylim([0, 0.1])
+    axis[0].set_ylim([0, 1])
+    axis[1].set_ylim([0, 0.1])
     axis[0].set_title("correlation")
     axis[0].boxplot((bv_corr, mh_corr), whis=whis, labels=labels)
     axis[1].set_title("p_values")
