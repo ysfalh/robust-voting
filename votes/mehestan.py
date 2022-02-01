c0ac102a685cb4e0962cb48ab7f04be67237cfd6
youssef <youssefallouah@gmail.com>
2022-02-01 11:27:20 +0100

    parallelize global scores computation + init_mehestan

--- votes/mehestan.py
+++ votes/mehestan.py
@@ -1,10 +1,7 @@
 """ Implementation of Mehestan """
 import numpy as np
 from votes.basic_vote import BasicVote
-# import multiprocessing as mp
 import multiprocess as mp
-import time
-from tqdm import tqdm
 
 
 def find_pair(mask, weights, ratings):
@@ -15,12 +12,26 @@ def find_pair(mask, weights, ratings):
         return sum((mask[:, i] & mask[:, j] & ((ratings[:, i] - ratings[:, j]) != 0)) * weights)
 
     best_weight, best_i, best_j = 0, 0, 0
-    for i in range(len(mask[0]) - 1):
-        for j in range(i + 1, len(mask[0])):
+    n_alternatives = mask.shape[1]
+    for i in range(n_alternatives - 1):
+        for j in range(i + 1, n_alternatives):
             new_weight = _count_ratings(mask, i, j, weights, ratings)
             if new_weight > best_weight:
                 best_weight, best_i, best_j = new_weight, i, j
-    return best_i, best_j
+    return best_i, best_j, best_weight
+
+
+def multi_find_pair(mask, weights, ratings, pool):
+    n_proc = pool._processes
+    n_alternatives = mask.shape[1]
+    alternatives_lists = [range(p * n_alternatives // n_proc, (p + 1) * n_alternatives // n_proc) for p in
+                          range(n_proc)]
+
+    def f(alt_list):
+        return find_pair(mask[:, alt_list], weights, ratings)
+
+    out = max(pool.map(f, alternatives_lists), key=lambda item: item[2])[:2]
+    return out
 
 
 class Mehestan(BasicVote):
@@ -51,12 +62,6 @@ class Mehestan(BasicVote):
         # print("S_n: {}".format(out * (max(ratings[0]) - min(ratings[0])) / abs(ratings[0, 0]-ratings[0, 1])))
         return out
 
-    def __compute_scalings(self, voter_list, ratings):
-        return [self.__learn_scaling(voter, ratings) for voter in voter_list]
-
-    def __compute_translations(self, voter_list, ratings, scalings):
-        return [self.__learn_translation(voter, ratings, scalings) for voter in voter_list]
-
     def __learn_translation(self, voter, ratings, scalings):
         scores, weights = [], []
         voter_alters = [i for i, x in enumerate(self.mask[voter, :]) if x == 1]
@@ -78,18 +83,25 @@ class Mehestan(BasicVote):
         # S = scalings[voter] * (max(ratings[0]) - min(ratings[0])) / abs(ratings[0, 0]-ratings[0, 1])
         return out
 
-    def __compute_factors(self, n_proc=1):
-        pool = mp.Pool(n_proc)
+    def __compute_scalings(self, voter_list):
+        return [self.__learn_scaling(voter, self.ratings) for voter in voter_list]
+
+    def __compute_translations(self, voter_list, scalings):
+        return [self.__learn_translation(voter, self.ratings, scalings) for voter in voter_list]
+
+    def __compute_factors(self, pool):
+        n_proc = pool._processes
         voter_lists = [range(p * self.n_voters // n_proc, (p + 1) * self.n_voters // n_proc) for p in range(n_proc)]
-        scalings = sum(pool.starmap(self.__compute_scalings, [(voter_lists[p], self.ratings) for p in range(n_proc)]), [])
+        scalings = sum(pool.map(self.__compute_scalings, [voter_lists[p] for p in range(n_proc)]), [])
         translations = sum(
-            pool.starmap(self.__compute_translations, [(voter_lists[p], self.ratings, scalings) for p in range(n_proc)]), [])
+            pool.starmap(self.__compute_translations, [(voter_lists[p], scalings) for p in range(n_proc)]), [])
 
         return scalings, translations
 
-    def __init_mehestan(self):
+    def __init_mehestan(self, pool):
         """ rescaling using most rated pair of alternatives """
-        a, b = find_pair(self.mask, self.voting_rights, self.ratings)
+        # a, b, _ = find_pair(self.mask, self.voting_rights, self.ratings)
+        a, b = multi_find_pair(self.mask, self.voting_rights, self.ratings, pool)
         # print("Pair chosen by Mehestan: {}".format((a, b)))
         for voter in range(self.n_voters):
             if self.mask[voter][a] and self.mask[voter][b]:
@@ -101,26 +113,17 @@ class Mehestan(BasicVote):
 
     def run(self):
         """ run voting algorithm """
-
+        pool = mp.Pool(self.n_proc)
         # mehestan normalisation
-        self.__init_mehestan()
+        self.__init_mehestan(pool)
 
-        out = np.zeros(self.n_alternatives)
         print("Mehestan: computing scaling and translation factors")
-        # scalings = [self.learn_scaling(voter, self.ratings) for voter in tqdm(range(self.n_voters))]
-        # translations = [self.learn_translation(voter, self.ratings, scalings) for voter in tqdm(range(self.n_voters))]
-        start_time = time.time()
-        scalings, translations = self.__compute_factors(n_proc=self.n_proc)
-        print("time: {}".format(time.time() - start_time))
+        scalings, translations = self.__compute_factors(pool)
 
         for voter in range(self.n_voters):
             self.ratings[voter, :] = scalings[voter] * self.ratings[voter, :] + translations[voter]
+
         print("Mehestan: computing global scores")
-        for alternative in tqdm(range(self.n_alternatives)):
-            scores = np.array([x for voter, x in enumerate(self.ratings[:, alternative])
-                               if self.mask[voter][alternative] != 0]).reshape(-1, 1)
-            weights = np.array(
-                [x for voter, x in enumerate(self.voting_rights) if self.mask[voter][alternative] != 0]).reshape(-1, 1)
-            out[alternative] = self.qr_median(scores, weights)
+        out = self.multi_compute_global_scores(pool)
 
         return out
