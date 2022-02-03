ee168875ddc044f9a4460c7f3fe0b7ce9dc04b38
oscar <voscar.vo@gmail.com>
2022-02-03 16:17:29 +0100

    minor fix

--- votes/basic_vote.py
+++ votes/basic_vote.py
@@ -76,8 +76,8 @@ class BasicVote(Vote):
         for voter in range(self.n_voters):
             self.ratings[voter] = self.transformation.sparse_apply(self.ratings[voter], self.mask[voter, :])
 
-        out = self.multi_compute_global_scores(pool, delta=self.delta)
-        out_noreg = self.multi_compute_global_scores(pool, delta=1e-6, noreg=noreg) if noreg else np.zeros(
+        out = self.multi_compute_global_scores(pool)
+        out_noreg = self.multi_compute_global_scores(pool, noreg=noreg) if noreg else np.zeros(
             self.n_alternatives)
 
         return out, out_noreg
